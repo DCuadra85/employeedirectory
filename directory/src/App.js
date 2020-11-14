ac8d4ab271c36322c8d02ad96c1358df8b90cb6f
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-11-14 14:31:14 -0800

    basic idea of button clicks, need to think about how to assign

--- directory/src/App.js
+++ directory/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import Wrapper from "./Wrapper"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  state = {
+    friends,
+    filter: ""
+  };
+
+  handleButtonClickAsc = (event) => {
+    event.preventDefault();
+    //console.log("reached btn click, ", this.state.friends)
+    this.sortEmployees(this.state.friends);
+  };
+
+  handleButtonClickDesc = (event) => {
+    event.preventDefault();
+    //console.log("reached btn click, ", this.state.friends)
+    this.sortReverseEmployees(this.state.friends);
+  };
+
+
+
+  render() {
+
+  }
 }
+
+
 
 export default App;
